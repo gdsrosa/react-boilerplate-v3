8eaa9ed5372cb2dfb904dbb9ed76903ad73fb6db
Gabriel Rosa <gabrieldsrosa@gmail.com>
2020-07-23 19:50:08 -0300

    feat: add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const wepack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exposts = ({ mode }) => ({
+  mode,
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    filename: 'build.js',
+    path: path.resolve('build'),
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new webpack.ProgressPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+});
