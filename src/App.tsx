b70a3b6bcb0821c2ab42777cd85a2966f41771e2
Gabriel Rosa <gabrieldsrosa@gmail.com>
2020-07-24 22:54:50 -0300

    feat: add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,28 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { RootState } from './store/types';
 import { handleSetMessage } from './store/modules/greet';
+import './App.scss';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
   const { message } = useSelector((state: RootState) => state.greetReducer);
 
   return (
-    <div>
+    <div className="App">
       Click on the button see a message!
       <button
         type="button"
-        onClick={() => dispatch(handleSetMessage('Hello!'))}
+        onClick={() =>
+          dispatch(handleSetMessage('Hello, explore the application go to'))
+        }
       >
         See message
       </button>
-      {message && <h2>{message}</h2>}
+      {message && (
+        <p>
+          {message} <code>App.tsx</code>{' '}
+        </p>
+      )}
     </div>
   );
 };
